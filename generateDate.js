a15f9a58b94dbcb53b25f60d8e6b20460a6dd900
Pratama Dimas <pratamadimas.172001@gmail.com>
2023-11-12 20:21:38 +0700

    Update meta.json

--- generateDate.js
+++ generateDate.js
@@ -1,6 +1,6 @@
 // Generate Date Library
 const CurrentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const DateLibs = new Date("2023-11-13 07:00");
+const DateLibs = new Date("2023-11-12 20:17");
 
 function generateUnixDate() {
   return DateLibs.valueOf();

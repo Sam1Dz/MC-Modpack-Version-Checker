6fe2889f71bcf8c23238e74a6502c0e3acb5ec75
Pratama Dimas <pratamadimas.pw@gmail.com>
2024-09-13 12:59:18 +0700

    Update

--- generateDate.js
+++ generateDate.js
@@ -1,6 +1,6 @@
 // Generate Date Library
 const CurrentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const DateLibs = new Date("2024-09-12 17:00");
+const DateLibs = new Date("2024-09-13 13:00");
 
 function generateUnixDate() {
   return DateLibs.valueOf();
